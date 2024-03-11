36cd2c4673a76b94c9e9a320f0ccee59ed921600
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2024-03-11 13:19:49 -0700

    quick update

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-import net from 'net';
+import cookie from 'cookie';
+import ejs from 'ejs';
+import fs from 'fs'
+import http from 'http';
 
 
-const port = 3000;
-const server = net.createServer((socket) => {
-    console.log("Connection establish: " + socket.remoteAddress)
+const server = http.createServer( (req ,res) =>{
     
-    socket.on()
-});
+    if (req.method == 'GET') {
+        if (req.url == '/') {
+            const loginPage = fs.readFileSync('./CRUD-GroceryApp1.1/views/mainAccessShell.ejs')
+            const renderEjs = ejs.render(loginPage, 'utf8')
 
+            res.end(loginPage)
+        }
+    }
+})
 
-
-server.listen(port, () => console.log("listening"));
+server.listen(3000, '127.0.0.1', () => console.log("Listening..."));
